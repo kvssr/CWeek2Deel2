b6636531bb3dd03301280f7362c2834f13e26ddc
kvssr <kevin.visser.info@gmail.com>
2017-11-20 16:49:16 +0100

    Add project files.

---
+++ Week2Deel1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week2Deel1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
